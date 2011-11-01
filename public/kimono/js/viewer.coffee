5e881a1012df17c390d16f9dd6513790fdaacb76
hitode909 <hitode909@gmail.com>
2011-11-01 10:00:01 +0900

    ツイート関連，表示中の地域と一致させる

--- public/kimono/js/viewer.coffee
+++ public/kimono/js/viewer.coffee
@@ -52,6 +52,7 @@ class Viewer
     city_code = $('select#city-selector').val()
 
     if city_code == "-1"           # 地域を選択 = -1
+      $('#indicator').show()
       this.getCurrentPositionAndPrint()
     else
       this.printWeather()
@@ -86,3 +87,53 @@ class Viewer
       $('#result #description').text report.daily.wDescription
       $('#result #max-temp').text report.daily.maxTemp
       $('#result #min-temp').text report.daily.minTemp
+
+    state_name = city_name.split(/\s+/)[0]
+    state_name = state_name.slice(0, state_name.length - 1)
+    this.appendWidget(state_name)
+    this.setTweetLink(state_name)
+
+  appendWidget: (state_name) ->
+    $('#widget-container').empty()
+    `new TWTR.Widget({
+    id: 'widget-container',
+    version: 2,
+    type: 'search',
+    search: state_name,
+    interval: 30000,
+    title: state_name,
+    subject: '',
+    width: 320,
+    height: 480,
+    theme: {
+      shell: {
+        background: '#98c6d1',
+        color: '#3c576e'
+      },
+      tweets: {
+        background: '#ffffff',
+        color: '#424242',
+        links: '#436a94'
+      }
+    },
+    features: {
+      scrollbar: false,
+      loop: false,
+      live: true,
+      behavior: 'all',
+    }
+  }).render().start();`
+
+  setTweetLink: (state_name) ->
+    url = "http://higashi-dance-network.appspot.com/bon/"
+    text = "盆のご案内です #盆 #盆#{state_name}"
+    share_url = "https://twitter.com/share?url=#{encodeURIComponent(url)}&text=#{encodeURIComponent(text)}"
+    $("a#share-tweet").attr
+      href: share_url
+
+
+
+
+
+
+
--- public/kimono/js/viewer.js
+++ public/kimono/js/viewer.js
@@ -55,6 +55,7 @@ Viewer = (function() {
     var city_code;
     city_code = $('select#city-selector').val();
     if (city_code === "-1") {
+      $('#indicator').show();
       return this.getCurrentPositionAndPrint();
     } else {
       return this.printWeather();
@@ -75,20 +76,64 @@ Viewer = (function() {
     });
   };
   Viewer.prototype.printWeather = function() {
-    var city, city_code, city_name, selected;
+    var city, city_code, city_name, selected, state_name;
     $('#indicator').show();
     selected = $('select#city-selector option:selected');
     city_code = selected.val();
     city_name = selected.text();
     city = this.kimono.getCityByCityCode(city_code);
     this.kimono.setLastCityCode(city_code);
-    return this.kimono.getWeatherReportForCity(city, function(report) {
+    this.kimono.getWeatherReportForCity(city, function(report) {
       $('#indicator').hide();
       $('#result #area').text(city_name);
       $('#result #date').text(report.daily.date);
       $('#result #description').text(report.daily.wDescription);
       $('#result #max-temp').text(report.daily.maxTemp);
       return $('#result #min-temp').text(report.daily.minTemp);
+    });
+    state_name = city_name.split(/\s+/)[0];
+    state_name = state_name.slice(0, state_name.length - 1);
+    this.appendWidget(state_name);
+    return this.setTweetLink(state_name);
+  };
+  Viewer.prototype.appendWidget = function(state_name) {
+    $('#widget-container').empty();
+    return new TWTR.Widget({
+    id: 'widget-container',
+    version: 2,
+    type: 'search',
+    search: state_name,
+    interval: 30000,
+    title: state_name,
+    subject: '',
+    width: 320,
+    height: 480,
+    theme: {
+      shell: {
+        background: '#98c6d1',
+        color: '#3c576e'
+      },
+      tweets: {
+        background: '#ffffff',
+        color: '#424242',
+        links: '#436a94'
+      }
+    },
+    features: {
+      scrollbar: false,
+      loop: false,
+      live: true,
+      behavior: 'all',
+    }
+  }).render().start();;
+  };
+  Viewer.prototype.setTweetLink = function(state_name) {
+    var share_url, text, url;
+    url = "http://higashi-dance-network.appspot.com/bon/";
+    text = "盆のご案内です #盆 #盆" + state_name;
+    share_url = "https://twitter.com/share?url=" + (encodeURIComponent(url)) + "&text=" + (encodeURIComponent(text));
+    return $("a#share-tweet").attr({
+      href: share_url
     });
   };
   return Viewer;
