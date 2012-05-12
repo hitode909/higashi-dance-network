f6624f484a98a38e9e1b3b97d65c308e8762d972
hitode909 <hitode909@gmail.com>
2012-05-12 15:48:49 +0900

    text page

---
+++ text.py
@@ -0,0 +1,61 @@
+import cgi
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.api import memcache, urlfetch
+import os
+import logging
+from google.appengine.ext import db
+
+class TextRecord(db.Model):
+    data       = db.TextProperty()
+    created_on = db.DateTimeProperty(auto_now_add = 1)
+
+    def path(self):
+        return "/text/" + str(self.key())
+
+class Page(webapp.RequestHandler):
+
+    def post(self):
+        if not self.request.get('data'):
+            logging.info('no data')
+            self.response.set_status(404)
+            self.response.out.write('no data')
+            return
+
+        record = TextRecord()
+        record.data = db.Text(self.request.get('data'))
+        record.put()
+        logging.info('POST')
+        logging.info(self.request.get('data'))
+        logging.info(record.path())
+        self.response.headers['Content-Type'] = "text/plain"
+        self.response.out.write(record.path())
+
+class GetPage(webapp.RequestHandler):
+    def get(self, key):
+        try:
+            record = TextRecord.get(db.Key(key))
+        except db.BadKeyError:
+            record = None
+
+        if not record:
+            self.response.set_status(404)
+            self.response.out.write('404')
+            return
+
+        self.response.headers['Content-Type'] = "text/plain"
+        self.response.out.write(record.data)
+        return
+
+application = webapp.WSGIApplication(
+                                     [
+                                      ('/text/', Page),
+                                      ('/text/(.+)', GetPage),
+                                     ],
+                                     debug=True)
+
+def main():
+  run_wsgi_app(application)
+
+if __name__ == "__main__":
+  main()
