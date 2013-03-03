4bc955850b3344fe9c8391eb16890ba6de1a1776
hitode909 <hitode909@gmail.com>
2013-03-03 12:49:38 +0900

    localize

--- public/kyotocamera/kyotocamera.coffee
+++ public/kyotocamera/kyotocamera.coffee
@@ -1,4 +1,22 @@
+document.body.style.opacity = '0.0'
+
 $ ->
+  localize = ->
+    language = (navigator.browserLanguage || navigator.language || navigator.userLanguage).substr(0,2).toLowerCase()
+    return if language is 'ja'
+
+    $('[data-english]').each ->
+      $element = $(this)
+      $element.text $element.attr('data-english')
+
+    $('title').text($('[data-english-title]').attr('data-english-title'))
+    $('img#logo').attr
+      src: 'service-logo-en.png'
+
+  do localize
+
+  $('body').animate({'opacity': 1.0}, 1000)
+
   $('.photo-item a').colorbox
     rel: 'gallery'
     returnFocus: false
--- public/kyotocamera/kyotocamera.js
+++ public/kyotocamera/kyotocamera.js
@@ -1,4 +1,26 @@
+document.body.style.opacity = '0.0';
 $(function() {
+  var localize;
+  localize = function() {
+    var language;
+    language = (navigator.browserLanguage || navigator.language || navigator.userLanguage).substr(0, 2).toLowerCase();
+    if (language === 'ja') {
+      return;
+    }
+    $('[data-english]').each(function() {
+      var $element;
+      $element = $(this);
+      return $element.text($element.attr('data-english'));
+    });
+    $('title').text($('[data-english-title]').attr('data-english-title'));
+    return $('img#logo').attr({
+      src: 'service-logo-en.png'
+    });
+  };
+  localize();
+  $('body').animate({
+    'opacity': 1.0
+  }, 1000);
   $('.photo-item a').colorbox({
     rel: 'gallery',
     returnFocus: false
