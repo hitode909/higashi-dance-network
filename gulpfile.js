3e68cf02d8172562479cc2fa7f9d6e8dabfa68ac
hitode909 <hitode909@gmail.com>
2016-07-18 14:46:50 +0900

    nam run build

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
 const gulp   = require('gulp');
 const coffee = require('gulp-coffee');
 
-gulp.task('kasanegi-coffee', () =>
+gulp.task('build', () =>
           gulp.src('public/kasanegi/js/*.coffee')
           .pipe(coffee({bare: true}))
           .pipe(gulp.dest('public/kasanegi/js'))
          );
 
 gulp.task('watch', function() {
-    gulp.watch('public/kasanegi/js/*.coffee', ['kasanegi-coffee']);
+    gulp.watch('public/kasanegi/js/*.coffee', ['build']);
 });
 
-gulp.task('default', ['kasanegi-coffee']);
+gulp.task('default', ['build']);
