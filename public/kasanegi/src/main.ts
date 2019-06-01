5e0cc4b2a9f742e34d1ef5db7c4409452ffcae7f
hitode909 <hitode909@gmail.com>
2019-06-01 20:46:02 +0900

    install sentry

--- public/kasanegi/src/main.ts
+++ public/kasanegi/src/main.ts
@@ -3,7 +3,13 @@ import { Viewer } from './viewer';
 import { WorkerLoader } from './WorkerLoader';
 
 document.addEventListener('DOMContentLoaded', () => {
-  let weather = new Weather();
+  try {
+    (window as any).Sentry.init({ dsn: 'https://a864b4ad35594708b2b2a6bb8f371565@sentry.io/1472798' });
+  } catch (error) {
+    console.warn(error);
+  }
+
+  let weather=new Weather();
   let viewer = new Viewer(weather);
   viewer.setup();
 
