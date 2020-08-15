5910a7ec184f26b5cc5d129954cbf8b4f10ed367
hitode909 <hitode909@gmail.com>
2020-08-15 15:33:53 +0900

    refactor

--- app.js
+++ app.js
@@ -4,6 +4,21 @@ const express = require('express');
 const fetch = require('node-fetch');
 const app = express();
 
+const mirror = async (res, url) => {
+  if (url.match(/meta/i)) {
+    console.log(`block ${url}`);
+    res.status(400).end();
+    return;
+  }
+
+  console.log(`fetch ${url}`);
+  const fetched = await fetch(url);
+  const buffer = await fetched.buffer();
+  res.status(fetched.status);
+  res.append('content-type', fetched.headers.get('content-type'));
+  res.end(buffer, 'binary');
+}
+
 app.get('/weather', async (req, res) => {
   let APIKEY = process.env['OPENWEATHERMAP_API_KEY'];
 
@@ -15,28 +30,15 @@ app.get('/weather', async (req, res) => {
 
   console.log(req.params);
   const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${encodeURIComponent(req.query.lat)}&lon=${encodeURIComponent(req.query.lon)}&exclude=current,minutely,hourly&units=metric&lang=ja&appid=${APIKEY}`;
-  const fetched = await fetch(url);
-  const body = await fetched.text();
-  res.status(fetched.status);
-  res.append('content-type', fetched.headers.get('content-type'));
-  res.send(body);
-
-  res.end();
+  await mirror(res, url);
 });
 
 app.get('/proxy/\*', async (req, res) => {
   console.log(req.originalUrl);
   const url = decodeURIComponent(req.originalUrl.replace(/^\/proxy\//, ''));
-  console.log(`fetch ${url}`);
-  if (url.match(/meta/i)) {
-    res.status(400).end();
-  }
-  const fetched = await fetch(url);
-  const body = await fetched.text();
-  res.status(fetched.status);
-  res.append('content-type', fetched.headers.get('content-type'));
-  res.send(body);
-  res.end();
+
+  await mirror(res, url);
+
 });
 
 app.use('/', express.static('public'));
