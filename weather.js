75194699c9eaaf42fde00ce49d47bfcf821adc74
hitode909 <hitode909@gmail.com>
2020-08-15 14:17:01 +0900

    serve files from Express

--- weather.js
+++ weather.js
@@ -5,24 +5,7 @@ const fetch = require('node-fetch');
 
 const app = express();
 
-const APIKEY = process.env['OPENWEATHERMAP_API_KEY'];
 
-if (!APIKEY) {
-  console.larn('OPENWEATHERMAP_API_KEY IS NOT SET')
-  process.exit(1);
-}
-
-app.get('/', async (req, res) => {
-  console.log(req.params);
-  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${encodeURIComponent(req.query.lat)}&lon=${encodeURIComponent(req.query.lon)}&exclude=current,minutely,hourly&units=metric&lang=ja&appid=${APIKEY}`;
-  const fetched = await fetch(url);
-  const body = await fetched.text();
-  res.status(fetched.status);
-  res.append('content-type', fetched.headers.get('content-type'));
-  res.send(body);
-
-  res.end();
-});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
